04ddd3e8fe94579a2a88958ff997033e6150b669
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-09-28 03:00:53 +0330

    Update m.cpp

--- m.cpp
+++ m.cpp
@@ -8,7 +8,6 @@
 
     std::vector<std::string> task;
     std::cout << "TM  : TASK MANAGER FOR MY SELF" <<  std::endl;
-    std::cout << "HOW TO USE : add doing reza" << std::endl;
     std::cout << "remove 1" << std::endl;
     if (task.size() != 0){
     for ( int i  = 0 ; i < task.size() ; i++){
